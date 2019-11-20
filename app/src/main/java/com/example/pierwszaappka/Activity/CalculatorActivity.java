a84726d7768d2f6399cf895eee98176a71ea7d37
tomaszk1991 <kubik.tomasz@o2.pl>
2019-11-20 17:48:41 +0100

    new class Calculator -code from activity moved to Calculator.class

--- app/src/main/java/com/example/pierwszaappka/Activity/CalculatorActivity.java
+++ app/src/main/java/com/example/pierwszaappka/Activity/CalculatorActivity.java
@@ -3,7 +3,6 @@ package com.example.pierwszaappka.Activity;
 import android.os.Bundle;
 
 
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -11,12 +10,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.pierwszaappka.Components.Calculator;
 import com.example.pierwszaappka.R;
 
 public class CalculatorActivity extends AppCompatActivity {
 
     private TextView calcResult;
-    private String calcResultString="";
+    private String calcResultString = "";
+    private Calculator calculator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,63 +37,18 @@ public class CalculatorActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        calcResult=findViewById(R.id.calcResult);
-        if (calcResultString.equals("")){
+        calcResult = findViewById(R.id.calcResult);
+        if (calcResultString.equals("")) {
             calcResult.setText("0");
-        }
-        else calcResult.setText(calcResultString);
+        } else calcResult.setText(calcResultString);
 
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     public void calcClick(View view) {
-
-        if (calcResultString.length()<15) {
-            switch (view.getId()) {
-
-                case R.id.zeroBtn:
-                    calcResultString += "0";
-                    break;
-                case R.id.oneBtn:
-                    calcResultString += "1";
-                    break;
-                case R.id.twoBtn:
-                    calcResultString += "2";
-                    break;
-                case R.id.threeBtn:
-                    calcResultString += "3";
-                    break;
-                case R.id.fourBtn:
-                    calcResultString += "4";
-                    break;
-                case R.id.fiveBtn:
-                    calcResultString += "5";
-                    break;
-                case R.id.sixBtn:
-                    calcResultString += "6";
-                    break;
-                case R.id.sevenBtn:
-                    calcResultString += "7";
-                    break;
-                case R.id.eightBtn:
-                    calcResultString += "8";
-                    break;
-                case R.id.nineBtn:
-                    calcResultString += "9";
-                    break;
-
-                default:
-                    Log.d("guzik", "nieprzypisany guzik" + view.getId());
-                    break;
-            }
-        }
-
-
-
-
-
-
+        calculator = new Calculator();
+        calcResultString = calculator.onClick(view, calcResultString);
         onResume();
 
     }
--- app/src/main/java/com/example/pierwszaappka/Components/Calculator.java
+++ app/src/main/java/com/example/pierwszaappka/Components/Calculator.java
@@ -1,13 +1,58 @@
 package com.example.pierwszaappka.Components;
 
+import android.util.Log;
+import android.view.View;
+
+import com.example.pierwszaappka.R;
+
 public class Calculator {
 
 
+    public Calculator() {
+    }
 
 
+    public String onClick(View view, String calcResultString) {
 
+        if (calcResultString.length() < 15) {
+            switch (view.getId()) {
 
+                case R.id.zeroBtn:
+                    calcResultString += "0";
+                    break;
+                case R.id.oneBtn:
+                    calcResultString += "1";
+                    break;
+                case R.id.twoBtn:
+                    calcResultString += "2";
+                    break;
+                case R.id.threeBtn:
+                    calcResultString += "3";
+                    break;
+                case R.id.fourBtn:
+                    calcResultString += "4";
+                    break;
+                case R.id.fiveBtn:
+                    calcResultString += "5";
+                    break;
+                case R.id.sixBtn:
+                    calcResultString += "6";
+                    break;
+                case R.id.sevenBtn:
+                    calcResultString += "7";
+                    break;
+                case R.id.eightBtn:
+                    calcResultString += "8";
+                    break;
+                case R.id.nineBtn:
+                    calcResultString += "9";
+                    break;
 
-
-
+                default:
+                    Log.d("guzik", "nieprzypisany guzik" + view.getId());
+                    break;
+            }
+        }
+        return calcResultString;
+    }
 }
